8416957cc60d6f73ef274e99242866cdc9299745
swhp <sukma.wardana@mail.com>
2017-02-19 22:20:25 +0700

    remove unnecessary annotations

--- src/main/java/id/swhp/javaee/library/config/LoggerProducer.java
+++ src/main/java/id/swhp/javaee/library/config/LoggerProducer.java
@@ -5,10 +5,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.inject.Named;
 import javax.inject.Singleton;
 
-@Named
 @Singleton
 public class LoggerProducer {
 
